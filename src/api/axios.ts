64860f1f7439fad1fe6209e1758deb54db11f2c2
Jiwoo <a20304645@gmail.com>
2023-06-10 17:55:52 +0900

    fix: baseURL

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://172.30.1.67:3000",
-  // baseURL: "http://api.github-resume.consistent.kr/",
+  // baseURL: "http://172.30.1.67:3000",
+  baseURL: "https://api.github-resume.consistent.kr/",
 });
 
 axios.interceptors.request.use(
