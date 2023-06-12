434650e2b35046fd849b32f46d59ba2c7fa95475
Jiwoo <a20304645@gmail.com>
2023-06-12 16:16:13 +0900

    fix: remove env

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const { DEV, VITE_PROD_BASE_URL } = import.meta.env;
+
 const instance = axios.create({
-  baseURL: "https://github-resume.api.consistent.kr",
+  baseURL: DEV ? "http://localhost:3000" : VITE_PROD_BASE_URL,
 });
 
 axios.interceptors.request.use(
