b790541909f93b07139d07c6b9449b7b173ee595
Panxoat <a20304645@gmail.com>
2023-07-22 20:10:38 +0900

    fix: 문의 메일 변경

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -10,8 +10,8 @@ export const Footer = ({ className }: { className?: string }) => {
     >
       <p className="text-[12px] text-[#393D50]">
         피드백 문의.{" "}
-        <a className="cursor-pointer underline">seongbeom_lee@kakao.com</a> | ©
-        2023 GitHub Resume 모든 권리 보유.
+        <a className="cursor-pointer underline">jhan0531@naver.com</a> | © 2023
+        GitHub Resume 모든 권리 보유.
       </p>
     </footer>
   );
